9abce0b7014efcfdc18f14a72712d37d81f0e730
amaryahwolf <115281632+amaryahwolf@users.noreply.github.com>
2022-12-27 15:11:29 -0800

    add employee, engineer, intern functions and questions array

--- index.js
+++ index.js
@@ -1,29 +1,60 @@
 // Packages needed for application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const employeeList = []
+const Employee = require('./lib/Employee')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const Manager = require('./lib/Manager')
 
 // Array of questions for user input
-const questions = [
+const managerQuestions = [
     {
         type: 'input',
         message: "Enter team manager's name:",
-        name: 'manager-name',
+        name: 'managerName',
     },
     {
         type: 'input',
         message: "Enter team manager's employee ID:",
-        name: 'manager-ID'
+        name: 'managerId'
     },
     {
         type: 'input',
         message: "Enter team manager's email address:",
-        name: 'manager-email'
+        name: 'managerEmail'
     },
     {
         type: 'input',
         message: "Enter team manager's office number:",
-        name: 'manager-office'
+        name: 'officeNumber'
     },
+]
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        message: "Enter Engineer's name:",
+        name: 'engineerName',
+    },
+    {
+        type: 'input',
+        message: "Enter engineer's employee ID:",
+        name: 'engineerId'
+    },
+    {
+        type: 'input',
+        message: "Enter engineer's email address:",
+        name: 'engineerEmail'
+    },
+    {
+        type: 'input',
+        message: "Enter engineer's github username:",
+        name: 'github'
+    },
+]
+
+const menuQuestions = [
     {
         type: 'list',
         message: 'Would you like to add an engineer, add an intern, or finish building your team?',
@@ -34,6 +65,24 @@ const questions = [
 
 // Function to generate HTML template literal
 function generateHtml(data) {
+    let employeeHtml = '';
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].getRole() === 'Manager') {
+            const managerHtml = `<div>
+        <p>${data[i].getName()}</p>
+        <p>${data[i].getId()}</p>
+        <p>${data[i].getEmail()}</p>
+        <p>${data[i].getOfficeNumber()}</p>
+        </div>`
+    employeeHtml = employeeHtml + managerHtml    
+    } else if (data[i].getRole() === 'Engineer') {
+        const engineerHtml = `<div>
+        <p>${data[i].getName()}</p>
+        <p>${data[i].getId()}</p>
+        <p>${data[i].getEmail()}</p>
+        <p>${data[i].getOfficeNumber()}</p>
+        </div>`
+    }
     return `<!DOCTYPE html>
     <html lang="en">
     <head>
@@ -43,14 +92,14 @@ function generateHtml(data) {
         <title>Team Profile</title>
     </head>
     <body>
-        
+        ${employeeHtml}
     </body>
     </html>`;
 }
 
 // Function to write HTML file
-function writetoFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
+function writetoFile() {
+    fs.writeFile('index.html', generateHtml(employeeList), (err) => {
         if(err) throw err 
         console.log("Input received!")
     }
@@ -58,15 +107,53 @@ function writetoFile(fileName, data) {
 }
 
 // Function to initialize app
-function init() {
+function getManager() {
     inquirer
-    .prompt (questions)
+    .prompt (managerQuestions)
     .then((response)=> {
-        writetoFile('index.html', generateHtml(response))
-        console.log(response)
+        const manager = new Manager(response.managerName, response.managerId, response.managerEmail, response.officeNumber)
+        employeeList.push(manager);
+        nextEmployee()
+    }
+    )
+}
+
+function nextEmployee() {
+    inquirer
+    .prompt (menuQuestions)
+    .then((response)=> {
+        if ('add an engineer' === response.menu) {
+            return getEngineer()
+        } else if  ('add an intern' === response.menu) {
+             return getIntern()   
+        } else {
+            return writetoFile()
+        }
+    }
+    )
+}
+
+function getEngineer() {
+    inquirer
+    .prompt (engineerQuestions)
+    .then((response)=> {
+       const engineer = new Engineer(response.engineerName, response.engineerId, response.engineerEmail, response.github)
+       employeeList.push(engineer)
+       nextEmployee()
+    }
+    )
+}
+
+function getIntern() {
+    inquirer
+    .prompt (internQuestions)
+    .then((response)=> {
+       const intern = new Engineer(response.internName, response.internId, response.internEmail, response.school)
+       employeeList.push(intern)
+       nextEmployee()
     }
     )
 }
 
 // Function call to initialize app
-init();
+getManager();
